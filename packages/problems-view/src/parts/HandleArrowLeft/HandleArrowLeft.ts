2a4a3df885d72b65241a6bf19c273cf47c687196
Le Vivilet <levivlet@pm.me>
2025-07-15 23:59:41 +0200

    feature: add arrowLeft

---
+++ packages/problems-view/src/parts/HandleArrowLeft/HandleArrowLeft.ts
@@ -0,0 +1,34 @@
+import type { Problem } from '../Problem/Problem.ts'
+import type { ProblemsState } from '../ProblemsState/ProblemsState.ts'
+import * as ProblemListItemType from '../ProblemListItemType/ProblemListItemType.ts'
+
+interface IndexResult {
+  readonly index: number
+  readonly newCollapsedUris: readonly string[]
+}
+
+const getArrowLeftNewFocusedIndex = (problems: readonly Problem[], collapsedUris: readonly string[], focusedIndex: number): IndexResult => {
+  for (let i = focusedIndex; i >= 0; i--) {
+    const problem = problems[i]
+    if (problem.listItemType !== ProblemListItemType.Item) {
+      return {
+        index: i,
+        newCollapsedUris: [...collapsedUris, problem.uri],
+      }
+    }
+  }
+  return {
+    index: 0,
+    newCollapsedUris: collapsedUris,
+  }
+}
+
+export const handleArrowLeft = (state: ProblemsState): ProblemsState => {
+  const { problems, focusedIndex, collapsedUris } = state
+  const { index, newCollapsedUris } = getArrowLeftNewFocusedIndex(problems, collapsedUris, focusedIndex)
+  return {
+    ...state,
+    focusedIndex: index,
+    collapsedUris: newCollapsedUris,
+  }
+}
