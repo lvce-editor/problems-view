8cd83cca822744e3697554021975bd52636375c3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-16 00:01:50 +0200

    feature: add strings (#13)

---
+++ packages/problems-view/src/parts/ProblemStrings/ProblemStrings.ts
@@ -0,0 +1,96 @@
+import * as I18NString from '@lvce-editor/i18n'
+
+/**
+ * @enum {string}
+ */
+const UiStrings = {
+  ClearFilters: 'Clear Filters',
+  Code: 'Code',
+  Copy: 'Copy',
+  CopyMessage: 'Copy Message',
+  File: 'File',
+  Filter: 'Filter',
+  LineColumn: '[Ln {PH1}, Col {PH2}]',
+  Message: 'Message',
+  NoProblemsDetected: 'No problems have been detected in the workspace.',
+  NoResultsFoundWithProvidedFilterCriteria: 'No results found with provided filter criteria.',
+  ProblemsDetected: 'Some problems have been detected in the workspace.',
+  ShowingOfItems: 'Showing {PH1} of {PH2} ',
+  Source: 'Source',
+  CollapseAll: 'Collapse All',
+  ViewAsTable: 'View as Table',
+  ViewAsList: 'View as List',
+}
+
+export const noProblemsDetected = (): string => {
+  return I18NString.i18nString(UiStrings.NoProblemsDetected)
+}
+
+export const getMessage = (problemsCount: number): string => {
+  if (problemsCount === 0) {
+    return I18NString.i18nString(UiStrings.NoProblemsDetected)
+  }
+  return I18NString.i18nString(UiStrings.ProblemsDetected)
+}
+
+export const atLineColumn = (line: number, column: number): string => {
+  return I18NString.i18nString(UiStrings.LineColumn, {
+    PH1: line,
+    PH2: column,
+  })
+}
+
+export const copy = (): string => {
+  return I18NString.i18nString(UiStrings.Copy)
+}
+
+export const clearFilter = (): string => {
+  return I18NString.i18nString(UiStrings.ClearFilters)
+}
+
+export const code = (): string => {
+  return I18NString.i18nString(UiStrings.Code)
+}
+
+export const message = (): string => {
+  return I18NString.i18nString(UiStrings.Message)
+}
+
+export const file = (): string => {
+  return I18NString.i18nString(UiStrings.File)
+}
+
+export const filter = (): string => {
+  return I18NString.i18nString(UiStrings.Filter)
+}
+
+export const showingOf = (visibleCount: number, totalCount: number): string => {
+  return I18NString.i18nString(UiStrings.ShowingOfItems, {
+    PH1: visibleCount,
+    PH2: totalCount,
+  })
+}
+
+export const source = (): string => {
+  return I18NString.i18nString(UiStrings.Source)
+}
+
+export const copyMessage = (): string => {
+  return I18NString.i18nString(UiStrings.CopyMessage)
+}
+
+export const noResultsFoundWithProvidedFilterCriteria = (): string => {
+  return I18NString.i18nString(UiStrings.NoResultsFoundWithProvidedFilterCriteria)
+}
+
+export const collapseAll = (): string => {
+  return I18NString.i18nString(UiStrings.CollapseAll)
+}
+
+export const viewAsList = (): string => {
+  return I18NString.i18nString(UiStrings.ViewAsList)
+}
+
+export const viewAsTable = (): string => {
+  return I18NString.i18nString(UiStrings.ViewAsTable)
+}
