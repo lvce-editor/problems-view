e8176e278ca5d8334404b0c21b777b4c7e5e1a12
Le Vivilet <levivlet@pm.me>
2025-07-16 00:07:41 +0200

    feature: update commandmap

--- packages/problems-view/src/parts/CommandMap/CommandMap.ts
+++ packages/problems-view/src/parts/CommandMap/CommandMap.ts
@@ -1,1 +1,16 @@
-export const commandMap = {}
+import { terminate } from '@lvce-editor/viewlet-registry'
+import * as Create from '../Create/Create.ts'
+import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
+import * as HandleArrowLeft from '../HandleArrowLeft/HandleArrowLeft.ts'
+import * as HandleArrowRight from '../HandleArrowRight/HandleArrowRight.ts'
+import * as Initialize from '../Initialize/Initialize.ts'
+import * as WrapCommand from '../ProblemsStates/ProblemsStates.ts'
+
+export const commandMap = {
+  'Problems.handleArrowLeft': WrapCommand.wrapCommand(HandleArrowLeft.handleArrowLeft),
+  'Problems.handleArrowRight': WrapCommand.wrapCommand(HandleArrowRight.handleArrowRight),
+  'Problems.create': Create.create,
+  'Problems.getKeyBindings': GetKeyBindings.getKeyBindings,
+  'Problems.terminate': terminate,
+  'Problems.initialize': Initialize.initialize,
+}
