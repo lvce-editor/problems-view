8d6ee42f6ce0422d2532b80dd99f4972281d6fd1
Le Vivilet <levivlet@pm.me>
2025-07-15 23:52:21 +0200

    feature: add keybindings

---
+++ packages/problems-view/src/parts/GetKeyBindings/GetKeyBindings.ts
@@ -0,0 +1,63 @@
+import { KeyCode } from '@lvce-editor/virtual-dom-worker'
+import type { KeyBinding } from '../KeyBinding/KeyBinding.ts'
+import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
+
+export const getKeyBindings = (): readonly KeyBinding[] => {
+  return [
+    {
+      key: KeyCode.DownArrow,
+      command: 'Problems.focusNext',
+      when: WhenExpression.FocusProblems,
+    },
+    {
+      key: KeyCode.UpArrow,
+      command: 'Problems.focusPrevious',
+      when: WhenExpression.FocusProblems,
+    },
+    {
+      key: KeyCode.Home,
+      command: 'Problems.focusFirst',
+      when: WhenExpression.FocusProblems,
+    },
+    {
+      key: KeyCode.PageUp,
+      command: 'Problems.focusFirst',
+      when: WhenExpression.FocusProblems,
+    },
+    {
+      key: KeyCode.PageDown,
+      command: 'Problems.focusLast',
+      when: WhenExpression.FocusProblems,
+    },
+    {
+      key: KeyCode.End,
+      command: 'Problems.focusLast',
+      when: WhenExpression.FocusProblems,
+    },
+    {
+      key: KeyCode.Space,
+      command: 'Problems.selectCurrent',
+      when: WhenExpression.FocusProblems,
+    },
+    {
+      key: KeyCode.Home,
+      command: 'Problems.focusFirst',
+      when: WhenExpression.FocusProblems,
+    },
+    {
+      key: KeyCode.End,
+      command: 'Problems.focusLast',
+      when: WhenExpression.FocusProblems,
+    },
+    {
+      key: KeyCode.LeftArrow,
+      command: 'Problems.handleArrowLeft',
+      when: WhenExpression.FocusProblems,
+    },
+    {
+      key: KeyCode.RightArrow,
+      command: 'Problems.handleArrowRight',
+      when: WhenExpression.FocusProblems,
+    },
+  ]
+}
