643c718c29a0b10a810a13ead2124f86e3d5c9f4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-16 10:16:33 +0200

    feature: update problem type (#38)

--- packages/problems-view/src/parts/Problem/Problem.ts
+++ packages/problems-view/src/parts/Problem/Problem.ts
@@ -3,4 +3,13 @@ export interface Problem {
   readonly uri: string
   readonly listItemType: number
   readonly source: string
+  readonly rowIndex: number
+  readonly columnIndex: number
+  readonly relativePath: string
+  readonly code: string | number
+  readonly type: string
+  readonly posInSet: number
+  readonly setSize: number
+  readonly level: number
+  readonly count: number
 }
