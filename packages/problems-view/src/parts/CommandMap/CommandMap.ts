d0c45d5b0c016e34f63f970f5e84864997000738
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-16 10:37:15 +0200

    feature: add diff2 (#47)
    * feature: add diff2
    
    * import
    
    * register command

--- packages/problems-view/src/parts/CommandMap/CommandMap.ts
+++ packages/problems-view/src/parts/CommandMap/CommandMap.ts
@@ -1,18 +1,24 @@
 import { terminate } from '@lvce-editor/viewlet-registry'
+import { copyMessage } from '../CopyMessage/CopyMessage.ts'
 import * as Create from '../Create/Create.ts'
+import { diff2 } from '../Diff2/Diff2.ts'
 import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
 import * as HandleArrowLeft from '../HandleArrowLeft/HandleArrowLeft.ts'
 import * as HandleArrowRight from '../HandleArrowRight/HandleArrowRight.ts'
 import * as Initialize from '../Initialize/Initialize.ts'
 import * as WrapCommand from '../ProblemsStates/ProblemsStates.ts'
+import { render2 } from '../Render2/Render2.ts'
 import { saveState } from '../SaveState/SaveState.ts'
 
 export const commandMap = {
   'Problems.create': Create.create,
+  'Problems.diff2': diff2,
   'Problems.getKeyBindings': GetKeyBindings.getKeyBindings,
   'Problems.handleArrowLeft': WrapCommand.wrapCommand(HandleArrowLeft.handleArrowLeft),
   'Problems.handleArrowRight': WrapCommand.wrapCommand(HandleArrowRight.handleArrowRight),
   'Problems.initialize': Initialize.initialize,
+  'Problems.render2': render2,
   'Problems.saveState': saveState,
   'Problems.terminate': terminate,
+  'Problems.copyMessage': copyMessage,
 }
--- packages/problems-view/src/parts/GetProblemsTableBodyVirtualDom/GetProblemsTableBodyVirtualDom.ts
+++ packages/problems-view/src/parts/GetProblemsTableBodyVirtualDom/GetProblemsTableBodyVirtualDom.ts
@@ -1,8 +1,8 @@
-import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker';
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { VisibleProblem } from '../VisibleProblem/VisibleProblem.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as GetProblemsTableRowVirtualDom from '../GetProblemsTableRowVirtualDom/GetProblemsTableRowVirtualDom.js'
+import * as GetProblemsTableRowVirtualDom from '../GetProblemsTableRowVirtualDom/GetProblemsTableRowVirtualDom.ts'
 
 export const getProblemsTableBodyVirtualDom = (problems: readonly VisibleProblem[]): readonly VirtualDomNode[] => {
   const dom = [
