f7a3ece01c2a075b7930282dd92a611f8632a12f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-16 15:53:08 +0200

    feature: add event listeners (#108)
    * feature: add event listeners
    
    * lint

--- packages/problems-view/src/parts/RenderEventListeners/RenderEventListeners.ts
+++ packages/problems-view/src/parts/RenderEventListeners/RenderEventListeners.ts
@@ -1,4 +1,24 @@
-export const renderEventListeners = (): readonly any[] => {
-  // TODO
-  return []
+import type { DomEventListener } from '@lvce-editor/virtual-dom-worker'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+
+export const renderEventListeners = (): readonly DomEventListener[] => {
+  return [
+    {
+      name: DomEventListenerFunctions.HandleBlur,
+      params: ['handleBlur'],
+    },
+    {
+      name: DomEventListenerFunctions.HandleContextMenu,
+      params: ['handleContextMenu', 'event.clientX', 'event.clientY'],
+      preventDefault: true,
+    },
+    {
+      name: DomEventListenerFunctions.HandleFilterInput,
+      params: ['handleFilterInput', 'event.target.value'],
+    },
+    {
+      name: DomEventListenerFunctions.HandleClearFilterClick,
+      params: ['clearFilter'],
+    },
+  ]
 }
