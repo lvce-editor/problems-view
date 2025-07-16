83a63f292f403ea4d1fbcafc83a43badc7b1eadd
Le Vivilet <levivlet@pm.me>
2025-07-16 10:25:27 +0200

    lint

--- packages/problems-view/src/parts/GetProblemsTableBodyVirtualDom/GetProblemsTableBodyVirtualDom.ts
+++ packages/problems-view/src/parts/GetProblemsTableBodyVirtualDom/GetProblemsTableBodyVirtualDom.ts
@@ -1,4 +1,5 @@
-import { VirtualDomElements, VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker';
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { VisibleProblem } from '../VisibleProblem/VisibleProblem.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetProblemsTableRowVirtualDom from '../GetProblemsTableRowVirtualDom/GetProblemsTableRowVirtualDom.js'
