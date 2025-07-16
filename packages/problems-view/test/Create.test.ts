f2b291ac8cf58277ccc857672f66492f784a1eb1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-16 12:17:51 +0200

    feature: add create test (#79)

---
+++ packages/problems-view/test/Create.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from '@jest/globals'
+import { create } from '../src/parts/Create/Create.ts'
+import { get } from '../src/parts/ProblemsStates/ProblemsStates.ts'
+
+test('create initializes state with correct default values', () => {
+  const id = 123
+  const uri = 'test://uri'
+  const x = 100
+  const y = 200
+  const width = 300
+  const height = 400
+  const args = { someArg: 'value' }
+  const parentUid = 456
+  create(id, uri, x, y, width, height, args, parentUid)
+  const stateTuple = get(id)
+  expect(stateTuple).toBeDefined()
+})
