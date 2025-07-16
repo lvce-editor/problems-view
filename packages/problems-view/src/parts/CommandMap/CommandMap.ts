de4918133c0402dc2072b8b229ca2f623e020aaf
Le Vivilet <levivlet@pm.me>
2025-07-16 12:28:40 +0200

    feature: use namespace imports

--- packages/problems-view/src/parts/CommandMap/CommandMap.ts
+++ packages/problems-view/src/parts/CommandMap/CommandMap.ts
@@ -1,28 +1,28 @@
-import { terminate } from '@lvce-editor/viewlet-registry'
-import { copyMessage } from '../CopyMessage/CopyMessage.ts'
+import * as ViewletRegistry from '@lvce-editor/viewlet-registry'
+import * as CopyMessage from '../CopyMessage/CopyMessage.ts'
 import * as Create from '../Create/Create.ts'
-import { diff2 } from '../Diff2/Diff2.ts'
-import { focusIndex } from '../FocusIndex/FocusIndex.ts'
+import * as Diff2 from '../Diff2/Diff2.ts'
+import * as FocusIndex from '../FocusIndex/FocusIndex.ts'
 import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
 import * as HandleArrowLeft from '../HandleArrowLeft/HandleArrowLeft.ts'
 import * as HandleArrowRight from '../HandleArrowRight/HandleArrowRight.ts'
 import * as Initialize from '../Initialize/Initialize.ts'
 import * as WrapCommand from '../ProblemsStates/ProblemsStates.ts'
-import { render2 } from '../Render2/Render2.ts'
-import { resize } from '../Resize/Resize.ts'
-import { saveState } from '../SaveState/SaveState.ts'
+import * as Render2 from '../Render2/Render2.ts'
+import * as Resize from '../Resize/Resize.ts'
+import * as SaveState from '../SaveState/SaveState.ts'
 
 export const commandMap = {
-  'Problems.copyMessage': copyMessage,
+  'Problems.copyMessage': CopyMessage.copyMessage,
   'Problems.create': Create.create,
-  'Problems.diff2': diff2,
-  'Problems.focusIndex': focusIndex,
+  'Problems.diff2': Diff2.diff2,
+  'Problems.focusIndex': FocusIndex.focusIndex,
   'Problems.getKeyBindings': GetKeyBindings.getKeyBindings,
   'Problems.handleArrowLeft': WrapCommand.wrapCommand(HandleArrowLeft.handleArrowLeft),
   'Problems.handleArrowRight': WrapCommand.wrapCommand(HandleArrowRight.handleArrowRight),
   'Problems.initialize': Initialize.initialize,
-  'Problems.render2': render2,
-  'Problems.resize': resize,
-  'Problems.saveState': saveState,
-  'Problems.terminate': terminate,
+  'Problems.render2': Render2.render2,
+  'Problems.resize': Resize.resize,
+  'Problems.saveState': SaveState.saveState,
+  'Problems.terminate': ViewletRegistry.terminate,
 }
