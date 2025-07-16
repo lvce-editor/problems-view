27baf1026edf9ccfe5570af24df5d24c995530b2
Le Vivilet <levivlet@pm.me>
2025-07-16 16:00:35 +0200

    fix: actions

--- packages/problems-view/src/parts/RenderActions/RenderActions.ts
+++ packages/problems-view/src/parts/RenderActions/RenderActions.ts
@@ -1,9 +1,10 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import type { ProblemsState } from '../ProblemsState/ProblemsState.ts'
 import * as GetActionsVirtualDom from '../GetActionsVirtualDom/GetActionsVirtualDom.ts'
 import * as ViewletProblemsActions from '../GetProblemActions/GetProblemActions.ts'
+import { get } from '../ProblemsStates/ProblemsStates.ts'
 
-export const renderActions = (newState: ProblemsState): readonly VirtualDomNode[] => {
+export const renderActions = (uid: number): readonly VirtualDomNode[] => {
+  const { newState } = get(uid)
   const actions = ViewletProblemsActions.getActions(newState)
   const dom = GetActionsVirtualDom.getActionsVirtualDom(actions)
   return dom
