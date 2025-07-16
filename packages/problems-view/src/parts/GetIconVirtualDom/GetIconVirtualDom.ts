1384876965b938cd52dc362a911ec8829ca444da
Le Vivilet <levivlet@pm.me>
2025-07-16 09:58:09 +0200

    feature: add icon dom

---
+++ packages/problems-view/src/parts/GetIconVirtualDom/GetIconVirtualDom.ts
@@ -0,0 +1,11 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker';
+import { AriaRoles, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+
+export const getIconVirtualDom = (icon: string, type = VirtualDomElements.Div): VirtualDomNode => {
+  return {
+    type,
+    className: `MaskIcon MaskIcon${icon}`,
+    role: AriaRoles.None,
+    childCount: 0,
+  }
+}
