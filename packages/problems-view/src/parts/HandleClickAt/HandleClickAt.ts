3585c9e5cf55b2b4e09c98de9174b5be79ea1d84
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-16 15:52:22 +0200

    feature: add click at command (#110)

---
+++ packages/problems-view/src/parts/HandleClickAt/HandleClickAt.ts
@@ -0,0 +1,21 @@
+import type { ProblemsState } from '../ProblemsState/ProblemsState.ts'
+import { focusIndex } from '../FocusIndex/FocusIndex.ts'
+import * as GetListIndex from '../GetListIndex/GetListIndex.ts'
+
+export const handleClickAt = (state: ProblemsState, eventX: number, eventY: number): ProblemsState => {
+  const { problems, x, y, itemHeight } = state
+
+  // TODO use functional focus rendering
+  // Focus.setFocus(FocusKey.Problems)
+  if (problems.length === 0) {
+    return focusIndex(state, -1)
+  }
+  const index = GetListIndex.getListIndex(eventX, eventY, x, y, 0, itemHeight)
+  if (index > problems.length) {
+    return focusIndex(state, -1)
+  }
+  return {
+    ...state,
+    focusedIndex: index,
+  }
+}
