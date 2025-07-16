9a352385217e074c57387c3bf6aed8f1afbbd8ed
Le Vivilet <levivlet@pm.me>
2025-07-16 10:08:49 +0200

    feature: add chevron dom

---
+++ packages/problems-view/src/parts/GetChevronVirtualDom/GetChevronVirtualDom.ts
@@ -0,0 +1,19 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker';
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+
+export const getChevronDownVirtualDom = (extraClassName = ''): VirtualDomNode => {
+  return {
+    type: VirtualDomElements.Div,
+    className: `${ClassNames.Chevron} MaskIconChevronDown ${extraClassName}`,
+    childCount: 0,
+  }
+}
+
+export const getChevronRightVirtualDom = (extraClassName = ''): VirtualDomNode => {
+  return {
+    type: VirtualDomElements.Div,
+    className: `${ClassNames.Chevron} MaskIconChevronRight ${extraClassName}`,
+    childCount: 0,
+  }
+}
