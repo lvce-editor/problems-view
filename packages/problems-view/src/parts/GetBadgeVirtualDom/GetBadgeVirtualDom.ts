962c8c12d19b32fda5f348ea80abbd69ff8b16c1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-16 12:18:24 +0200

    feature: format code (#80)

--- packages/problems-view/src/parts/GetBadgeVirtualDom/GetBadgeVirtualDom.ts
+++ packages/problems-view/src/parts/GetBadgeVirtualDom/GetBadgeVirtualDom.ts
@@ -1,4 +1,4 @@
-import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker';
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 
 export const getBadgeVirtualDom = (className: string, count: number): readonly VirtualDomNode[] => {
--- packages/problems-view/src/parts/GetProblemsTableHeaderVirtualDom/GetProblemsTableHeaderVirtualDom.ts
+++ packages/problems-view/src/parts/GetProblemsTableHeaderVirtualDom/GetProblemsTableHeaderVirtualDom.ts
@@ -1,4 +1,4 @@
-import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker';
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as ProblemStrings from '../ProblemStrings/ProblemStrings.ts'
