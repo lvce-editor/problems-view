695c10ac4f304998f1552603a3fe450182b6dc39
Le Vivilet <levivlet@pm.me>
2025-07-16 10:28:28 +0200

    feature: add renderItems

---
+++ packages/problems-view/src/parts/GetRenderer/GetRenderer.ts
@@ -0,0 +1,13 @@
+import type { ProblemsState } from '../ProblemsState/ProblemsState.ts'
+import type { Renderer } from '../Renderer/Renderer.ts'
+import * as DiffType from '../DiffType/DiffType.ts'
+import * as RenderItems from '../RenderItems/RenderItems.ts'
+
+export const getRenderer = (diffType: number): Renderer<ProblemsState> => {
+  switch (diffType) {
+    case DiffType.RenderItems:
+      return RenderItems.renderItems
+    default:
+      throw new Error('unknown renderer')
+  }
+}
