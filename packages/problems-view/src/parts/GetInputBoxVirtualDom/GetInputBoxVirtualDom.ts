025f6ef7c35d8cb1384ec4476257aa8a4bc3f4d6
Le Vivilet <levivlet@pm.me>
2025-07-16 21:43:53 +0200

    use name

--- packages/problems-view/src/parts/GetInputBoxVirtualDom/GetInputBoxVirtualDom.ts
+++ packages/problems-view/src/parts/GetInputBoxVirtualDom/GetInputBoxVirtualDom.ts
@@ -1,7 +1,6 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as InputName from '../InputName/InputName.ts'
 
 export const getInputBoxVirtualDom = (name: string, onInput: string, placeholder: string): VirtualDomNode => {
   return {
@@ -9,7 +8,7 @@ export const getInputBoxVirtualDom = (name: string, onInput: string, placeholder
     className: ClassNames.InputBox,
     autocapitalize: 'off',
     autocorrect: 'off',
-    name: InputName.Filter,
+    name,
     onInput: onInput,
     placeholder: placeholder,
     spellcheck: false,
