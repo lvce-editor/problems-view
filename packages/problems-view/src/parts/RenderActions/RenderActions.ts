aae4618cd3f027f6f65407f0877e88536c88bc9a
Le Vivilet <levivlet@pm.me>
2025-07-16 15:44:31 +0200

    feature: actions dom

--- packages/problems-view/src/parts/RenderActions/RenderActions.ts
+++ packages/problems-view/src/parts/RenderActions/RenderActions.ts
@@ -1,12 +1,10 @@
-import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker';
-import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import type { ProblemsState } from '../ProblemsState/ProblemsState.ts'
+import * as GetActionsVirtualDom from '../GetActionsVirtualDom/GetActionsVirtualDom.js'
+import * as ViewletProblemsActions from '../GetProblemActions/GetProblemActions.ts'
 
-export const renderActions = (): readonly VirtualDomNode[] => {
-  return [
-    {
-      type: VirtualDomElements.Div,
-      childCount: 1,
-    },
-    text('not implemented'),
-  ]
+export const renderActions = (newState: ProblemsState): readonly VirtualDomNode[] => {
+  const actions = ViewletProblemsActions.getActions(newState)
+  const dom = GetActionsVirtualDom.getActionsVirtualDom(actions)
+  return dom
 }
