233090985ffce3f88ae8b93d2e9e3c8c1f25899e
Le Vivilet <levivlet@pm.me>
2025-07-16 10:09:01 +0200

    feature: add badge dom

---
+++ packages/problems-view/src/parts/GetBadgeVirtualDom/GetBadgeVirtualDom.ts
@@ -0,0 +1,13 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker';
+import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+
+export const getBadgeVirtualDom = (className: string, count: number): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: `Badge ${className}`,
+      childCount: 1,
+    },
+    text(`${count}`),
+  ]
+}
