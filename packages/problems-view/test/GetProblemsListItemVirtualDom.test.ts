39a96bbb988f2aec307fb20321a89bc3e93c80ca
Le Vivilet <levivlet@pm.me>
2025-07-16 14:34:12 +0200

    feature: simplify list item test

--- packages/problems-view/test/GetProblemsListItemVirtualDom.test.ts
+++ packages/problems-view/test/GetProblemsListItemVirtualDom.test.ts
@@ -1,5 +1,7 @@
 import { test, expect } from '@jest/globals'
+import { VirtualDomElements, AriaRoles } from '@lvce-editor/virtual-dom-worker'
 import type { VisibleProblem } from '../src/parts/VisibleProblem/VisibleProblem.ts'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
 import { getProblemVirtualDom } from '../src/parts/GetProblemsListItemVirtualDom/GetProblemsListItemVirtualDom.ts'
 import * as ProblemListItemType from '../src/parts/ProblemListItemType/ProblemListItemType.ts'
 
@@ -33,14 +35,58 @@ test('getProblemVirtualDom returns correct dom for Expanded', () => {
     isEven: false,
   }
   const dom = getProblemVirtualDom(problem)
-  expect(dom[0]).toMatchObject({
-    className: expect.stringContaining('ProblemSelected'),
-    ariaExpanded: true,
-    role: expect.any(String),
-  })
-  expect(dom.some((el: any) => el.className && el.className.includes('Chevron'))).toBe(true)
-  expect(dom.some((el: any) => el.src === 'icon-ts')).toBe(true)
-  expect(dom.some((el: any) => typeof el === 'object' && el.className && el.className.includes('Badge'))).toBe(true)
+  const expectedDom = [
+    {
+      type: VirtualDomElements.Div,
+      className: `${ClassNames.Problem} ${ClassNames.ProblemSelected}`,
+      childCount: 5,
+      paddingLeft: '1rem',
+      ariaPosInSet: 1,
+      ariaSetSize: 1,
+      ariaLevel: 1,
+      ariaExpanded: true,
+      ariaSelected: true,
+      role: AriaRoles.TreeItem,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: `${ClassNames.Chevron} MaskIconChevronDown `,
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Img,
+      className: ClassNames.FileIcon,
+      src: 'icon-ts',
+      role: AriaRoles.None,
+      childCount: 0,
+    },
+    {
+      type: 12,
+      text: '/path/to/file.ts',
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.LabelDetail,
+      childCount: 1,
+    },
+    {
+      type: 12,
+      text: 'file.ts',
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: `Badge ${ClassNames.ProblemBadge}`,
+      childCount: 1,
+    },
+    {
+      type: 12,
+      text: '2',
+      childCount: 0,
+    },
+  ]
+  expect(dom).toEqual(expectedDom)
 })
 
 test('getProblemVirtualDom returns correct dom for Collapsed', () => {
@@ -54,13 +100,58 @@ test('getProblemVirtualDom returns correct dom for Collapsed', () => {
     isEven: true,
   }
   const dom = getProblemVirtualDom(problem)
-  expect(dom[0]).toMatchObject({
-    ariaExpanded: false,
-    role: expect.any(String),
-  })
-  expect(dom.some((el: any) => el.className && el.className.includes('Chevron'))).toBe(true)
-  expect(dom.some((el: any) => el.src === 'icon-ts')).toBe(true)
-  expect(dom.some((el: any) => typeof el === 'object' && el.className && el.className.includes('Badge'))).toBe(true)
+  const expectedDom = [
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.Problem,
+      childCount: 5,
+      paddingLeft: '1rem',
+      ariaPosInSet: 1,
+      ariaSetSize: 1,
+      ariaLevel: 1,
+      ariaExpanded: false,
+      ariaSelected: false,
+      role: AriaRoles.TreeItem,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: `${ClassNames.Chevron} MaskIconChevronRight `,
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Img,
+      className: ClassNames.FileIcon,
+      src: 'icon-ts',
+      role: AriaRoles.None,
+      childCount: 0,
+    },
+    {
+      type: 12,
+      text: '/path/to/file.ts',
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.LabelDetail,
+      childCount: 1,
+    },
+    {
+      type: 12,
+      text: 'file.ts',
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: `Badge ${ClassNames.ProblemBadge}`,
+      childCount: 1,
+    },
+    {
+      type: 12,
+      text: '2',
+      childCount: 0,
+    },
+  ]
+  expect(dom).toEqual(expectedDom)
 })
 
 test('getProblemVirtualDom returns correct dom for Item without filter highlight', () => {
@@ -74,13 +165,50 @@ test('getProblemVirtualDom returns correct dom for Item without filter highlight
     isEven: false,
   }
   const dom = getProblemVirtualDom(problem)
-  expect(dom[0]).toMatchObject({
-    className: expect.stringContaining('Problem'),
-    role: expect.any(String),
-  })
-  expect(dom.some((el: any) => el.className && el.className.includes('ProblemsIcon'))).toBe(true)
-  expect(dom.some((el: any) => typeof el === 'object' && el.className && el.className.includes('Label'))).toBe(true)
-  expect(dom.some((el: any) => typeof el === 'object' && el.className && el.className.includes('ProblemAt'))).toBe(true)
+  const expectedDom = [
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.Problem,
+      childCount: 3,
+      paddingLeft: '2rem',
+      ariaPosInSet: 1,
+      ariaSetSize: 1,
+      ariaLevel: 1,
+      ariaSelected: false,
+      role: AriaRoles.TreeItem,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: `${ClassNames.ProblemsIcon} ${ClassNames.ProblemsErrorIcon}`,
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.Label,
+      childCount: 1,
+    },
+    {
+      type: 12,
+      text: 'Syntax error',
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Span,
+      className: ClassNames.ProblemAt,
+      childCount: 2,
+    },
+    {
+      type: 12,
+      text: 'TypeScript(TS100) ',
+      childCount: 0,
+    },
+    {
+      type: 12,
+      text: '[Ln 10, Col 5]',
+      childCount: 0,
+    },
+  ]
+  expect(dom).toEqual(expectedDom)
 })
 
 test('getProblemVirtualDom returns correct dom for Item with filter highlight', () => {
@@ -96,6 +224,63 @@ test('getProblemVirtualDom returns correct dom for Item with filter highlight', 
     isEven: false,
   }
   const dom = getProblemVirtualDom(problem)
-  expect(dom.some((el: any) => el.className && el.className.includes('Highlight'))).toBe(true)
-  expect(dom.some((el: any) => typeof el === 'object' && el.className && el.className.includes('ProblemAt'))).toBe(true)
+  const expectedDom = [
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.Problem,
+      childCount: 3,
+      paddingLeft: '2rem',
+      ariaPosInSet: 1,
+      ariaSetSize: 1,
+      ariaLevel: 1,
+      ariaSelected: false,
+      role: AriaRoles.TreeItem,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: `${ClassNames.ProblemsIcon} ${ClassNames.ProblemsErrorIcon}`,
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.Label,
+      childCount: 3,
+    },
+    {
+      type: 12,
+      text: '',
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.Highlight,
+      childCount: 1,
+    },
+    {
+      type: 12,
+      text: 'Syntax',
+      childCount: 0,
+    },
+    {
+      type: 12,
+      text: ' error',
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Span,
+      className: ClassNames.ProblemAt,
+      childCount: 2,
+    },
+    {
+      type: 12,
+      text: 'TypeScript(TS100) ',
+      childCount: 0,
+    },
+    {
+      type: 12,
+      text: '[Ln 10, Col 5]',
+      childCount: 0,
+    },
+  ]
+  expect(dom).toEqual(expectedDom)
 })
