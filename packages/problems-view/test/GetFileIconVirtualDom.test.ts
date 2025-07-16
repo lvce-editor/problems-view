800213840b86388f13de9b604525631cd8fe1ec2
Le Vivilet <levivlet@pm.me>
2025-07-16 12:10:02 +0200

    test

--- packages/problems-view/test/GetFileIconVirtualDom.test.ts
+++ packages/problems-view/test/GetFileIconVirtualDom.test.ts
@@ -2,18 +2,14 @@ import { test, expect } from '@jest/globals'
 import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
 import { getFileIconVirtualDom } from '../src/parts/GetFileIconVirtualDom/GetFileIconVirtualDom.ts'
 
-// These values are assumed based on typical conventions
-const IMG_TYPE = 3 // VirtualDomElements.Img
-const ARIA_NONE = 0 // AriaRoles.None
-
 test('getFileIconVirtualDom returns correct virtual dom node', () => {
   const icon = 'test-icon.png'
   const result = getFileIconVirtualDom(icon)
   expect(result).toEqual({
-    type: IMG_TYPE,
+    type: 17,
     className: ClassNames.FileIcon,
     src: icon,
-    role: ARIA_NONE,
+    role: 'none',
     childCount: 0,
   })
 })
