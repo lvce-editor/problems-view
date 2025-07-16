0c3b914bf0dc9553fca3ac46f4dd3f6cad5f8d46
Le Vivilet <levivlet@pm.me>
2025-07-16 22:10:48 +0200

    feature: register filter input command

--- packages/problems-view/src/parts/CommandMap/CommandMap.ts
+++ packages/problems-view/src/parts/CommandMap/CommandMap.ts
@@ -18,29 +18,31 @@ import { renderActions } from '../RenderActions/RenderActions.ts'
 import { renderEventListeners } from '../RenderEventListeners/RenderEventListeners.ts'
 import * as Resize from '../Resize/Resize.ts'
 import * as SaveState from '../SaveState/SaveState.ts'
+import * as HandleFilterInput from '../HandleFilterInput/HandleFilterInput.ts'
 import { viewAsList } from '../ViewAsList/ViewAsList.ts'
 import { viewAsTable } from '../ViewAsTable/ViewAsTable.ts'
 
 export const commandMap = {
   'Problems.copyMessage': CopyMessage.copyMessage,
-  'Problems.renderEventListeners': renderEventListeners,
   'Problems.create': Create.create,
   'Problems.diff2': Diff2.diff2,
   'Problems.focusIndex': FocusIndex.focusIndex,
+  'Problems.getCommandIds': GetCommandIds.getCommandIds,
   'Problems.getKeyBindings': GetKeyBindings.getKeyBindings,
   'Problems.handleArrowLeft': WrapCommand.wrapCommand(HandleArrowLeft.handleArrowLeft),
   'Problems.handleArrowRight': WrapCommand.wrapCommand(HandleArrowRight.handleArrowRight),
+  'Problems.handleClickAt': WrapCommand.wrapCommand(handleClickAt),
+  'Problems.handleContextMenu': handleContextMenu,
+  'Problems.handleFilterInput': WrapCommand.wrapCommand(HandleFilterInput.handleFilterInput),
+  'Problems.handleIconThemeChange': handleIconThemeChange,
   'Problems.initialize': Initialize.initialize,
   'Problems.loadContent': WrapCommand.wrapCommand(loadContent),
   'Problems.render2': Render2.render2,
   'Problems.renderActions': renderActions,
+  'Problems.renderEventListeners': renderEventListeners,
   'Problems.resize': Resize.resize,
   'Problems.saveState': SaveState.saveState,
   'Problems.terminate': ViewletRegistry.terminate,
-  'Problems.handleIconThemeChange': handleIconThemeChange,
-  'Problems.handleContextMenu': handleContextMenu,
-  'Problems.viewAsTable': viewAsTable,
   'Problems.viewAsList': viewAsList,
-  'Problems.handleClickAt': WrapCommand.wrapCommand(handleClickAt),
-  'Problems.getCommandIds': GetCommandIds.getCommandIds,
+  'Problems.viewAsTable': viewAsTable,
 }
