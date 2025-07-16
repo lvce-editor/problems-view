37fd633988903dcb2ac022da0a557f6d0ad339b3
Le Vivilet <levivlet@pm.me>
2025-07-16 12:21:37 +0200

    feature: simplify array tests

--- packages/problems-view/test/GetProblemsNoProblemsFoundVirtualDom.test.ts
+++ packages/problems-view/test/GetProblemsNoProblemsFoundVirtualDom.test.ts
@@ -5,32 +5,54 @@ import { getProblemsNoProblemsFoundVirtualDom } from '../src/parts/GetProblemsNo
 
 test('getProblemsNoProblemsFoundVirtualDom with empty filterValue', () => {
   const dom = getProblemsNoProblemsFoundVirtualDom('')
-  expect(dom[0]).toEqual({
-    type: VirtualDomElements.Div,
-    className: ClassNames.Message,
-    childCount: 1,
-  })
-  expect(dom[1]).toMatchObject({ text: 'No problems have been detected in the workspace.' })
+  const expectedArray = [
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.Message,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: 'No problems have been detected in the workspace.',
+      childCount: 0,
+    },
+  ]
+  expect(dom).toEqual(expectedArray)
 })
 
 test('getProblemsNoProblemsFoundVirtualDom with non-empty filterValue', () => {
   const dom = getProblemsNoProblemsFoundVirtualDom('foo')
-  expect(dom[0]).toEqual({
-    type: VirtualDomElements.Div,
-    className: ClassNames.Message,
-    childCount: 3,
-  })
-  expect(dom[1]).toEqual({
-    type: VirtualDomElements.Span,
-    childCount: 1,
-  })
-  expect(dom[2]).toMatchObject({ text: 'No results found with provided filter criteria.' })
-  expect(dom[3]).toEqual({
-    type: VirtualDomElements.A,
-    className: ClassNames.MessageAction,
-    childCount: 1,
-    onClick: expect.any(String),
-  })
-  expect(dom[4]).toMatchObject({ text: 'Clear Filters' })
-  expect(dom[5]).toMatchObject({ text: '.' })
+  const expectedArray = [
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.Message,
+      childCount: 3,
+    },
+    {
+      type: VirtualDomElements.Span,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: 'No results found with provided filter criteria.',
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.A,
+      className: ClassNames.MessageAction,
+      childCount: 1,
+      onClick: expect.any(String),
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: 'Clear Filters',
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: '.',
+      childCount: 0,
+    },
+  ]
+  expect(dom).toEqual(expectedArray)
 })
--- packages/problems-view/test/GetProblemsTableHeaderVirtualDom.test.ts
+++ packages/problems-view/test/GetProblemsTableHeaderVirtualDom.test.ts
@@ -5,43 +5,62 @@ import { getProblemsTableHeaderVirtualDom } from '../src/parts/GetProblemsTableH
 
 test('getProblemsTableHeaderVirtualDom returns correct dom structure', () => {
   const dom = getProblemsTableHeaderVirtualDom()
-  expect(dom[0]).toEqual({
-    type: VirtualDomElements.Div,
-    className: ClassNames.ProblemsTableHeader,
-    childCount: 1,
-  })
-  expect(dom[1]).toEqual({
-    type: VirtualDomElements.Div,
-    className: ClassNames.ProblemsTableRow,
-    childCount: 5,
-  })
-  expect(dom[2]).toEqual({
-    type: VirtualDomElements.Div,
-    className: ClassNames.ProblemsTableRowItem,
-    childCount: 0,
-  })
-  expect(dom[3]).toEqual({
-    type: VirtualDomElements.Div,
-    className: ClassNames.ProblemsTableRowItem,
-    childCount: 1,
-  })
-  expect(dom[5]).toEqual({
-    type: VirtualDomElements.Div,
-    className: ClassNames.ProblemsTableRowItem,
-    childCount: 1,
-  })
-  expect(dom[7]).toEqual({
-    type: VirtualDomElements.Div,
-    className: ClassNames.ProblemsTableRowItem,
-    childCount: 1,
-  })
-  expect(dom[9]).toEqual({
-    type: VirtualDomElements.Div,
-    className: ClassNames.ProblemsTableRowItem,
-    childCount: 1,
-  })
-  expect(dom[4]).toMatchObject({ text: 'Code' })
-  expect(dom[6]).toMatchObject({ text: 'Message' })
-  expect(dom[8]).toMatchObject({ text: 'File' })
-  expect(dom[10]).toMatchObject({ text: 'Source' })
+  const expectedArray = [
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ProblemsTableHeader,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ProblemsTableRow,
+      childCount: 5,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ProblemsTableRowItem,
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ProblemsTableRowItem,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: 'Code',
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ProblemsTableRowItem,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: 'Message',
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ProblemsTableRowItem,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: 'File',
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ProblemsTableRowItem,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Text,
+      text: 'Source',
+      childCount: 0,
+    },
+  ]
+  expect(dom).toEqual(expectedArray)
 })
