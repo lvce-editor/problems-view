a4ef18f1ab8e601b31af4f576426da9397d805f7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-16 12:27:31 +0200

    feature: use namespace imports (#83)

--- packages/problems-view/test/GetTreeItemIndent.test.ts
+++ packages/problems-view/test/GetTreeItemIndent.test.ts
@@ -1,18 +1,18 @@
 import { test, expect } from '@jest/globals'
-import { getTreeItemIndent } from '../src/parts/GetTreeItemIndent/GetTreeItemIndent.ts'
+import * as GetTreeItemIndent from '../src/parts/GetTreeItemIndent/GetTreeItemIndent.ts'
 
 test('getTreeItemIndent returns correct indent for depth 1', () => {
-  expect(getTreeItemIndent(1)).toBe('1rem')
+  expect(GetTreeItemIndent.getTreeItemIndent(1)).toBe('1rem')
 })
 
 test('getTreeItemIndent returns correct indent for depth 0', () => {
-  expect(getTreeItemIndent(0)).toBe('0rem')
+  expect(GetTreeItemIndent.getTreeItemIndent(0)).toBe('0rem')
 })
 
 test('getTreeItemIndent returns correct indent for depth 5', () => {
-  expect(getTreeItemIndent(5)).toBe('5rem')
+  expect(GetTreeItemIndent.getTreeItemIndent(5)).toBe('5rem')
 })
 
 test('getTreeItemIndent returns correct indent for negative depth', () => {
-  expect(getTreeItemIndent(-2)).toBe('-2rem')
+  expect(GetTreeItemIndent.getTreeItemIndent(-2)).toBe('-2rem')
 })
