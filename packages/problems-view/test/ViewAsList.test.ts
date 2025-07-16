eca76e8f2eafada701cf114fa99f11f8ceb91d9b
Le Vivilet <levivlet@pm.me>
2025-07-16 16:08:15 +0200

    t

--- packages/problems-view/test/ViewAsList.test.ts
+++ packages/problems-view/test/ViewAsList.test.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@jest/globals'
-import { viewAsList } from '../src/parts/ViewAsList/ViewAsList.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as ProblemsViewMode from '../src/parts/ProblemsViewMode/ProblemsViewMode.ts'
+import { viewAsList } from '../src/parts/ViewAsList/ViewAsList.ts'
 
 test('viewAsList should set viewMode to List', () => {
   const state = createDefaultState()
--- packages/problems-view/test/ViewAsTable.test.ts
+++ packages/problems-view/test/ViewAsTable.test.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@jest/globals'
-import { viewAsTable } from '../src/parts/ViewAsTable/ViewAsTable.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as ProblemsViewMode from '../src/parts/ProblemsViewMode/ProblemsViewMode.ts'
+import { viewAsTable } from '../src/parts/ViewAsTable/ViewAsTable.ts'
 
 test('viewAsTable should set viewMode to Table', () => {
   const state = createDefaultState()
