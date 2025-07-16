55241ff8238e861609cd001f5be56556f77c0792
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-16 10:11:32 +0200

    feature: add icon dom (#36)

---
+++ packages/problems-view/src/parts/GetProblemsIconVirtualDom/GetProblemsIconVirtualDom.ts
@@ -0,0 +1,20 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker';
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DiagnosticType from '../DiagnosticType/DiagnosticType.ts'
+
+export const getProblemsIconVirtualDom = (type: string): VirtualDomNode => {
+  // TODO use mergeclassnames
+  if (type === DiagnosticType.Warning) {
+    return {
+      type: VirtualDomElements.Div,
+      className: `${ClassNames.ProblemsIcon} ${ClassNames.ProblemsWarningIcon}`,
+      childCount: 0,
+    }
+  }
+  return {
+    type: VirtualDomElements.Div,
+    className: `${ClassNames.ProblemsIcon} ${ClassNames.ProblemsErrorIcon}`,
+    childCount: 0,
+  }
+}
