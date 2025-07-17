d81bdf2a8550aebb8a659233fabfbfcd48427291
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-17 13:21:54 +0200

    feature: simplify no problems found dom (#145)
    * feature: simplify no problems found dom
    
    * t

--- packages/problems-view/src/parts/GetProblemsItemsVirtualDom/GetProblemsItemsVirtualDom.ts
+++ packages/problems-view/src/parts/GetProblemsItemsVirtualDom/GetProblemsItemsVirtualDom.ts
@@ -1,13 +1,17 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { VisibleProblem } from '../VisibleProblem/VisibleProblem.ts'
+import { getNoResultsWithFilterVirtualDom } from '../GetNoResultsWithFilterVirtualDom/GetNoResultsWithFilterVirtualDom.ts'
 import * as GetProblemsListVirtualDom from '../GetProblemsListVirtualDom/GetProblemsListVirtualDom.ts'
 import * as GetProblemsNoProblemsFoundVirtualDom from '../GetProblemsNoProblemsFoundVirtualDom/GetProblemsNoProblemsFoundVirtualDom.ts'
 import * as GetProblemsTableVirtualDom from '../GetProblemsTableVirtualDom/GetProblemsTableVirtualDom.ts'
 import * as ProblemsViewMode from '../ProblemsViewMode/ProblemsViewMode.ts'
 
 export const getProblemsVirtualDom = (viewMode: number, problems: readonly VisibleProblem[], filterValue: string): readonly VirtualDomNode[] => {
+  if (problems.length === 0 && filterValue) {
+    return getNoResultsWithFilterVirtualDom()
+  }
   if (problems.length === 0) {
-    return GetProblemsNoProblemsFoundVirtualDom.getProblemsNoProblemsFoundVirtualDom(filterValue)
+    return GetProblemsNoProblemsFoundVirtualDom.getProblemsNoProblemsFoundVirtualDom()
   }
   if (viewMode === ProblemsViewMode.Table) {
     return GetProblemsTableVirtualDom.getProblemsTableVirtualDom(problems)
