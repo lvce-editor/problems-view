12156b7bb44984fb8fafb52dfb7df8947daaa35e
Le Vivilet <levivlet@pm.me>
2025-07-17 22:36:06 +0200

    feature: query problems from editor worker

--- packages/problems-view/src/parts/GetProblems/GetProblems.ts
+++ packages/problems-view/src/parts/GetProblems/GetProblems.ts
@@ -1,14 +1,17 @@
 import type { ProblemsResult } from '../ProblemsResult/ProblemsResult.ts'
 import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
+import { toProblems } from '../ToProblems/ToProblems.ts'
+
+// TODO send event listener to editor, asking to send problem updates to this worker
 
 export const getProblems = async (state: any): Promise<ProblemsResult> => {
   try {
-    // TODO send event listener to editor, asking to send problem updates to this worker
     // @ts-ignore
-    await EditorWorker.invoke('Editor.getProblems')
-    // TODO ask editor worker for problems
+    const diagnostics = await EditorWorker.invoke('Editor.getProblems')
+    // @ts-ignore
+    const problems = toProblems(diagnostics)
     return {
-      problems: [],
+      problems,
       error: '',
     }
   } catch (error) {
