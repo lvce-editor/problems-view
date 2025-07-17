bbce4caa3a5dcfc5c4d17b0aac2ebc81b6def020
Le Vivilet <levivlet@pm.me>
2025-07-17 10:52:51 +0200

    feature: add renderfilterValue

---
+++ packages/problems-view/src/parts/RenderFilterValue/RenderFilterValue.ts
@@ -0,0 +1,7 @@
+import type { ProblemsState } from '../ProblemsState/ProblemsState.ts'
+import type { ViewletCommand } from '../ViewletCommand/ViewletCommand.ts'
+
+export const renderFilterValue = (oldState: ProblemsState, newState: ProblemsState): ViewletCommand => {
+  // TODO
+  return ['']
+}
