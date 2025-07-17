5046f468f40c42b822c8600a44ec1fd98e8762ad
Le Vivilet <levivlet@pm.me>
2025-07-17 13:24:12 +0200

    feature: render error message if available

--- packages/problems-view/src/parts/RenderItems/RenderItems.ts
+++ packages/problems-view/src/parts/RenderItems/RenderItems.ts
@@ -4,9 +4,9 @@ import * as GetProblemsVirtualDom from '../GetProblemsVirtualDom/GetProblemsVirt
 import * as GetVisibleProblems from '../GetVisibleProblems/GetVisibleProblems.ts'
 
 export const renderItems = (oldState: ProblemsState, newState: ProblemsState): ViewletCommand => {
-  const { problems, width, smallWidthBreakPoint, collapsedUris, focusedIndex, viewMode, filterValue } = newState
+  const { problems, width, smallWidthBreakPoint, collapsedUris, focusedIndex, viewMode, filterValue, message } = newState
   const visible = GetVisibleProblems.getVisibleProblems(problems, collapsedUris, focusedIndex, filterValue)
   const isSmall = width <= smallWidthBreakPoint
-  const dom = GetProblemsVirtualDom.getProblemsVirtualDom(viewMode, visible, filterValue, isSmall)
+  const dom = GetProblemsVirtualDom.getProblemsVirtualDom(viewMode, visible, filterValue, isSmall, message)
   return ['Viewlet.setDom2', dom]
 }
