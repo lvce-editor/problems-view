02eff9d07db7ce7519e603dcdc699f678352f371
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-17 22:54:36 +0200

    feature: add handleButtonClick (#154)

---
+++ packages/problems-view/src/parts/HandleClickButton/HandleClickButton.ts
@@ -0,0 +1,5 @@
+import type { ProblemsState } from '../ProblemsState/ProblemsState.ts'
+
+export const handleClickButton = async (state: ProblemsState, name: string): Promise<ProblemsState> => {
+  return state
+}
