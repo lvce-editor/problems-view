9213fd183c14bf0eba48899a18b93d208201bae6
Le Vivilet <levivlet@pm.me>
2025-07-17 10:50:33 +0200

    feature: add input source property

--- packages/problems-view/src/parts/HandleFilterInput/HandleFilterInput.ts
+++ packages/problems-view/src/parts/HandleFilterInput/HandleFilterInput.ts
@@ -1,8 +1,10 @@
 import type { ProblemsState } from '../ProblemsState/ProblemsState.ts'
+import * as InputSource from '../InputSource/InputSource.ts'
 
-export const handleFilterInput = (state: ProblemsState, value: string): ProblemsState => {
+export const handleFilterInput = (state: ProblemsState, value: string, inputSource = InputSource.Script): ProblemsState => {
   return {
     ...state,
     filterValue: value,
+    inputSource,
   }
 }
