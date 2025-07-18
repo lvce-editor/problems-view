147b1450cf026ec6f386638f80bef73ab7b55e37
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-18 10:09:06 +0200

    feature: improve table e2e test (#162)

--- packages/e2e/src/problems.view-as-table.ts
+++ packages/e2e/src/problems.view-as-table.ts
@@ -1,29 +1,40 @@
+import { Test } from '@lvce-editor/test-with-playwright'
+
 export const name = 'problems.view-as-table'
 
 export const skip = 1
 
-export const test = async ({ Main, FileSystem, Workspace, Extension, Locator, Panel, expect }) => {
+export const test: Test = async ({ Main, Command, FileSystem, Workspace, Extension, Locator, Panel, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/file1.xyz`, 'content 1')
-
   await Workspace.setPath(tmpDir)
   // @ts-ignore
-  await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
-
+  await Extension.addWebExtension(new URL(`../fixtures/problems.one-problem`, import.meta.url).toString())
   await Main.openUri(`${tmpDir}/file1.xyz`)
-  await Panel.open()
-
-  // assert
+  await Panel.open('Problems')
   const problemsView = Locator('.Problems')
   await expect(problemsView).toBeVisible()
-
   const problems = Locator('.Problem')
   await expect(problems).toHaveCount(2)
-
   const firstProblem = problems.nth(0)
   await expect(firstProblem).toHaveText('file1.xyz1')
-
   const secondProblem = problems.nth(1)
   await expect(secondProblem).toHaveText('error 1xyz [Ln 0, Col 0]')
+
+  // act
+  await Command.execute('Problems.viewAsTable')
+
+  // assert
+  const table = Locator('.ProblemsTable')
+  await expect(table).toBeVisible()
+  const tableBody = Locator('.ProblemsTableBody')
+  const cellCode = tableBody.locator('.ProblemsTableRowItem').nth(1)
+  await expect(cellCode).toHaveText('xyz ')
+  const cellMessage = tableBody.locator('.ProblemsTableRowItem').nth(2)
+  await expect(cellMessage).toHaveText('error 1 ')
+  const cellFile = tableBody.locator('.ProblemsTableRowItem').nth(3)
+  await expect(cellFile).toHaveText('memfs://workspace/file1.xyz')
+  const cellSource = tableBody.locator('.ProblemsTableRowItem').nth(4)
+  await expect(cellSource).toHaveText('xyz')
 }
