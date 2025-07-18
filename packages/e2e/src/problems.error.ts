47b890560b37c82c597ecccc15d8528afe564a84
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-18 10:15:53 +0200

    feature: add problems error e2e test (#167)

---
+++ packages/e2e/src/problems.error.ts
@@ -0,0 +1,23 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'problems.error'
+
+export const skip = 1
+
+export const test: Test = async ({ Main, FileSystem, Workspace, Extension, Locator, Panel, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/file1.xyz`, 'content 1')
+  await Workspace.setPath(tmpDir)
+  // @ts-ignore
+  await Extension.addWebExtension(new URL(`../fixtures/${name}`, import.meta.url).toString())
+  await Main.openUri(`${tmpDir}/file1.xyz`)
+
+  // act
+  await Panel.open('Problems')
+
+  // assert
+  const problemsView = Locator('.Problems')
+  await expect(problemsView).toBeVisible()
+  await expect(problemsView).toHaveText(`Error: Failed to execute diagnostic provider: TypeError: x is not a function`)
+}
