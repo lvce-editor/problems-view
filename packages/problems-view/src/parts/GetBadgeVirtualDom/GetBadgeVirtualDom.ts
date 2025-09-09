e6bfd674c67d2712cb5f10bac2262e90c79512f8
Le Vivilet <levivilet@pm.me>
2025-09-09 13:24:28 +0200

    feature: use mergeClassNames helper function (#182)

--- packages/problems-view/src/parts/GetBadgeVirtualDom/GetBadgeVirtualDom.ts
+++ packages/problems-view/src/parts/GetBadgeVirtualDom/GetBadgeVirtualDom.ts
@@ -1,11 +1,11 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import { mergeClassNames, text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 
 export const getBadgeVirtualDom = (className: string, count: number): readonly VirtualDomNode[] => {
   return [
     {
       type: VirtualDomElements.Div,
-      className: `Badge ${className}`,
+      className: mergeClassNames('Badge', className) + (className === '' ? ' ' : ''),
       childCount: 1,
     },
     text(`${count}`),
