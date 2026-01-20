5d2057da03ad13ec88037bd0519573bba09ab25d
Le Vivilet <levivilet@pm.me>
2026-01-20 14:31:16 +0100

    feature: register getMenuEntries

--- packages/problems-view/src/parts/ProblemsStates/ProblemsStates.ts
+++ packages/problems-view/src/parts/ProblemsStates/ProblemsStates.ts
@@ -1,4 +1,4 @@
 import * as ViewletRegistry from '@lvce-editor//viewlet-registry'
 import type { ProblemsState } from '../ProblemsState/ProblemsState.ts'
 
-export const { get, set, wrapCommand } = ViewletRegistry.create<ProblemsState>()
+export const { get, set, wrapCommand, wrapGetter } = ViewletRegistry.create<ProblemsState>()
