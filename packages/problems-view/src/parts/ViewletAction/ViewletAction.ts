490d058b1f2b132d20eb049aae46a6602ff7f1af
Le Vivilet <levivilet@pm.me>
2026-01-20 14:47:13 +0100

    fix

--- packages/problems-view/src/parts/ViewletAction/ViewletAction.ts
+++ packages/problems-view/src/parts/ViewletAction/ViewletAction.ts
@@ -2,7 +2,7 @@ export interface ViewletAction {
   readonly badgeText?: string
   readonly command: string
   readonly icon?: string
-  readonly id: string
+  readonly id: string | number
   readonly placeholder?: string
   readonly type: number
   readonly value?: string
