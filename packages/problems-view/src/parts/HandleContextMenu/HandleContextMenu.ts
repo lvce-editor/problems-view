122962e36e1187c2ff84628986b87dc3282e7bff
Le Vivilet <levivilet@pm.me>
2026-01-20 13:44:44 +0100

    feature: add menu types (#208)
    * feature: add menu types
    
    * fix
    
    * register

--- packages/problems-view/src/parts/HandleContextMenu/HandleContextMenu.ts
+++ packages/problems-view/src/parts/HandleContextMenu/HandleContextMenu.ts
@@ -1,5 +1,6 @@
 import type { ProblemsState } from '../ProblemsState/ProblemsState.ts'
 
 export const handleContextMenu = async (state: ProblemsState, eventX: number, eventY: number): Promise<ProblemsState> => {
+  // TODO
   return state
 }
