ba1708a8c836fcb9a29a5e9bda81c58dbf26e4ec
Le Vivilet <levivilet@pm.me>
2026-01-20 14:50:59 +0100

    feature: simplify getCommandIds

--- packages/problems-view/src/parts/Listen/Listen.ts
+++ packages/problems-view/src/parts/Listen/Listen.ts
@@ -2,8 +2,10 @@ import * as CommandMap from '../CommandMap/CommandMap.ts'
 import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
 import { initializeEditorWorkerRpc } from '../InitializeEditorWorkerRpc/InitializeEditorWorkerRpc.ts'
 import { initializeRendererWorker } from '../InitializeRendererWorker/InitializeRendererWorker.ts'
+import { registerCommands } from '../ProblemsStates/ProblemsStates.ts'
 
 export const listen = async (): Promise<void> => {
   Object.assign(CommandMapRef.commandMapRef, CommandMap.commandMap)
+  registerCommands(CommandMap.commandMap)
   await Promise.all([initializeRendererWorker(), initializeEditorWorkerRpc()])
 }
