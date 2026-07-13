bc4c54e3bb508b3983fbcedf89c7dad718c6e4c3
Le Vivilet <levivilet@pm.me>
2026-07-13 10:25:57 +0200

    chore: update eslint and eslint config to latest (#257)
    * chore: update eslint and eslint config to latest
    
    * chore: sync package-lock for eslint update
    
    * fix: update lint rules and workflows

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,13 @@ export default [
   {
     rules: {
       '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+      'e2e/no-inline-nth-in-expect': 'off',
+      'e2e/prefer-import-meta-resolve': 'off',
+      'sonarjs/assertions-in-tests': 'off',
+      'sonarjs/prefer-specific-assertions': 'off',
       'unicorn/no-immediate-mutation': 'off',
+      'unicorn/no-useless-template-literals': 'off',
+      'unicorn/prefer-url-href': 'off',
     },
   },
 ]
