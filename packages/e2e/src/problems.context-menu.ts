59d76dee9b9d6b91bc0309770d55d32880659c77
Le Vivilet <levivilet@pm.me>
2026-07-16 06:37:19 +0000

    feature: add problems view e2e coverage

--- packages/e2e/src/problems.context-menu.ts
+++ packages/e2e/src/problems.context-menu.ts
@@ -1,15 +1,15 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const name = 'problems.filter-menu'
+export const name = 'problems.context-menu'
 
-export const test: Test = async ({ Command, expect, Locator, Panel, Problems }) => {
+export const test: Test = async ({ Command, expect, Locator, Panel }) => {
   // arrange
   await Panel.open('Problems')
-  await Problems.show()
   const problemsView = Locator('.Viewlet.Problems')
   await expect(problemsView).toBeVisible()
+
   // act
-  await Command.execute('Problems.handleClickMoreFilters', 0, 0)
+  await Command.execute('Problems.handleContextMenu', 0, 0)
 
   // assert
   const menu = Locator('.Menu')
