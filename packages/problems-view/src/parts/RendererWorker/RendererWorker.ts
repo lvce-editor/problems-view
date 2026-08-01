9b4c01a49748ff479cf6dfe5341795c582dd24af
Le Vivilet <levivilet@pm.me>
2026-08-01 15:11:25 +0000

    feature: update eslint dependencies

--- packages/problems-view/src/parts/RendererWorker/RendererWorker.ts
+++ packages/problems-view/src/parts/RendererWorker/RendererWorker.ts
@@ -1,3 +1,18 @@
+import type { Rpc } from '@lvce-editor/rpc'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
-export const { getActiveEditorId, sendMessagePortToEditorWorker, set, writeClipBoardText } = RendererWorker
+export const getActiveEditorId = (): Promise<number> => {
+  return RendererWorker.getActiveEditorId()
+}
+
+export const sendMessagePortToEditorWorker = (port: MessagePort, rpcId: number): Promise<void> => {
+  return RendererWorker.sendMessagePortToEditorWorker(port, rpcId)
+}
+
+export const set = (rpc: Rpc): void => {
+  RendererWorker.set(rpc)
+}
+
+export const writeClipBoardText = (text: string): Promise<void> => {
+  return RendererWorker.writeClipBoardText(text)
+}
