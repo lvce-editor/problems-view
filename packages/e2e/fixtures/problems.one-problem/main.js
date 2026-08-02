ae72d7c532312fb6abac22d393b49fbd2291943b
Le Vivilet <levivilet@pm.me>
2026-08-02 15:38:01 -0700

    fix: enable one problem e2e test (#286)

--- packages/e2e/fixtures/problems.one-problem/main.js
+++ packages/e2e/fixtures/problems.one-problem/main.js
@@ -1,4 +1,7 @@
+import { activate as activateExtensionApi, registerDiagnosticProvider } from '@lvce-editor/api'
+
 const diagnosticProvider = {
+  id: 'xyz-diagnostics',
   languageId: 'xyz',
   provideDiagnostics(textDocument, offset) {
     return [
@@ -6,13 +9,15 @@ const diagnosticProvider = {
         uri: textDocument.uri,
         rowIndex: 0,
         columnIndex: 0,
+        endRowIndex: 0,
+        endColumnIndex: 0,
         message: 'error 1',
         source: 'xyz',
+        type: 'error',
       },
     ]
   },
 }
 
-export const activate = () => {
-  vscode.registerDiagnosticProvider(diagnosticProvider)
-}
+await activateExtensionApi()
+registerDiagnosticProvider(diagnosticProvider)
