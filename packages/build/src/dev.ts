b1374a893a602cbd86ff167fb66adabb21320f7a
Le Vivilet <levivilet@pm.me>
2026-08-03 04:41:43 -0700

    fix: build diagnostics fixture before tests (#293)

--- packages/build/src/dev.ts
+++ packages/build/src/dev.ts
@@ -1,7 +1,9 @@
 import { execa } from 'execa'
+import { buildE2eExtensions } from './buildE2eExtensions.ts'
 import { root } from './root.ts'
 
 const main = async (): Promise<void> => {
+  await buildE2eExtensions()
   void execa(`npm`, ['run', 'build:watch'], {
     cwd: root,
     stdio: 'inherit',
