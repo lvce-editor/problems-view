a527f6cb70ece35f7ee5dde2996d9ce968bc6a86
Le Vivilet <levivilet@pm.me>
2026-08-03 22:21:33 +0000

    fix: make file icon e2e assertion portable

--- packages/e2e/src/problems.file-icon.ts
+++ packages/e2e/src/problems.file-icon.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'problems.file-icon'
 
-export const test: Test = async ({ BaseUrl, expect, Extension, FileSystem, IconTheme, Locator, Main, Panel, Workspace }) => {
+export const test: Test = async ({ expect, Extension, FileSystem, IconTheme, Locator, Main, Panel, Workspace }) => {
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/file1.xyz`, 'content 1')
   await Workspace.setPath(tmpDir)
@@ -13,8 +13,6 @@ export const test: Test = async ({ BaseUrl, expect, Extension, FileSystem, IconT
   await Main.openUri(`${tmpDir}/file1.xyz`)
   await Panel.openProblems()
 
-  const fileIcon = Locator('.Problem').nth(0).locator('.FileIcon')
-  const baseUrl = BaseUrl.getBaseUrl()
+  const fileIcon = Locator('.Problem').nth(0).locator('.FileIcon[src$="/icons/default_file.svg"]')
   await expect(fileIcon).toBeVisible()
-  await expect(fileIcon).toHaveAttribute('src', `${baseUrl}packages/extension-host-worker-tests/fixtures/sample.icon-theme/icons/default_file.svg`)
 }
