2ad5883d2f60b029969d3c50a2b50c6d58f7f651
Le Vivilet <levivilet@pm.me>
2026-08-07 22:40:50 -0700

    fix: focus editor when clicking problem (#298)
    * fix: focus editor when clicking problem
    
    * fix: order problem click test imports
    
    * fix: set cursor after opening problem

---
+++ packages/problems-view/src/parts/HandleProblemClick/HandleProblemClick.ts
@@ -0,0 +1,33 @@
+import type { ProblemsState } from '../ProblemsState/ProblemsState.ts'
+import * as GetVisibleProblems from '../GetVisibleProblems/GetVisibleProblems.ts'
+import { handleClickAt } from '../HandleClickAt/HandleClickAt.ts'
+import * as ProblemListItemType from '../ProblemListItemType/ProblemListItemType.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
+
+export const handleProblemClick = async (state: ProblemsState, eventX: number, eventY: number): Promise<ProblemsState> => {
+  const newState = handleClickAt(state, eventX, eventY)
+  const { focusedIndex } = newState
+  if (focusedIndex < 0) {
+    return newState
+  }
+  const { collapsedUris, fileIconCache, filterValue, problems, viewMode } = newState
+  const visibleProblems = GetVisibleProblems.getVisibleProblems(
+    problems,
+    fileIconCache,
+    collapsedUris,
+    focusedIndex,
+    filterValue,
+    focusedIndex,
+    focusedIndex + 1,
+    viewMode,
+  )
+  const problem = visibleProblems[0]
+  if (!problem || problem.listItemType !== ProblemListItemType.Item) {
+    return newState
+  }
+  const { columnIndex, rowIndex, uri } = problem
+  await RendererWorker.openUri(uri, true)
+  await RendererWorker.focusEditor()
+  await RendererWorker.setEditorCursor(rowIndex, columnIndex)
+  return newState
+}
---
+++ packages/problems-view/test/HandleProblemClick.test.ts
@@ -0,0 +1,83 @@
+import { expect, test } from '@jest/globals'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+import type { ProblemsState } from '../src/parts/ProblemsState/ProblemsState.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import { handleProblemClick } from '../src/parts/HandleProblemClick/HandleProblemClick.ts'
+import * as ProblemListItemType from '../src/parts/ProblemListItemType/ProblemListItemType.ts'
+import * as ProblemsViewMode from '../src/parts/ProblemsViewMode/ProblemsViewMode.ts'
+
+test('opens and focuses the clicked problem at its position', async () => {
+  using rendererRpc = RendererWorker.registerMockRpc({
+    'Editor.cursorSet': async () => {},
+    'Main.focus': async () => {},
+    'Main.openUri': async () => {},
+  })
+  const state: ProblemsState = {
+    ...createDefaultState(),
+    itemHeight: 22,
+    problems: [
+      {
+        code: '',
+        columnIndex: 7,
+        count: 0,
+        fileName: 'test.ts',
+        level: 2,
+        listItemType: ProblemListItemType.Item,
+        message: 'problem',
+        posInSet: 1,
+        relativePath: '',
+        rowIndex: 4,
+        setSize: 1,
+        source: 'test',
+        type: 'error',
+        uri: 'file:///workspace/test.ts',
+      },
+    ],
+    viewMode: ProblemsViewMode.List,
+    width: 800,
+  }
+
+  const result = await handleProblemClick(state, 50, 11)
+
+  expect(result.focusedIndex).toBe(0)
+  expect(rendererRpc.invocations).toEqual([
+    ['Main.openUri', { focus: true, uri: 'file:///workspace/test.ts' }],
+    ['Main.focus'],
+    ['Editor.cursorSet', 4, 7],
+  ])
+})
+
+test('does not open a file when clicking a problem group', async () => {
+  using rendererRpc = RendererWorker.registerMockRpc({
+    'Main.openUri': async () => {},
+  })
+  const state: ProblemsState = {
+    ...createDefaultState(),
+    itemHeight: 22,
+    problems: [
+      {
+        code: '',
+        columnIndex: 0,
+        count: 1,
+        fileName: 'test.ts',
+        level: 1,
+        listItemType: ProblemListItemType.Expanded,
+        message: '',
+        posInSet: 1,
+        relativePath: '',
+        rowIndex: 0,
+        setSize: 1,
+        source: '',
+        type: '',
+        uri: 'file:///workspace/test.ts',
+      },
+    ],
+    viewMode: ProblemsViewMode.List,
+    width: 800,
+  }
+
+  const result = await handleProblemClick(state, 50, 11)
+
+  expect(result.focusedIndex).toBe(0)
+  expect(rendererRpc.invocations).toEqual([])
+})
