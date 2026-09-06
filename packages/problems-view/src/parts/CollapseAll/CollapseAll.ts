9f387212903ee85b4be442f0695020b207c221cc
Le Vivilet <levivilet@pm.me>
2026-09-06 23:18:15 +0200

    feature: simplify code

--- packages/problems-view/src/parts/CollapseAll/CollapseAll.ts
+++ packages/problems-view/src/parts/CollapseAll/CollapseAll.ts
@@ -1,9 +1,13 @@
 import type { ProblemsState } from '../ProblemsState/ProblemsState.ts'
 import * as ProblemListItemType from '../ProblemListItemType/ProblemListItemType.ts'
 
+const isNotItem = (problem:Problem):boolean=>{
+  return problem.listItemType !== ProblemListItemType.Item
+}
+
 export const collapseAll = (state: ProblemsState): ProblemsState => {
   const { problems } = state
-  const collapsedUris = problems.filter((problem) => problem.listItemType !== ProblemListItemType.Item).map((problem) => problem.uri)
+  const collapsedUris = problems.filter(isNotItem).map((problem) => problem.uri)
   return {
     ...state,
     collapsedUris: [...new Set(collapsedUris)],
