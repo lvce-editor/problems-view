97575f6776062135d276d7d2501a9530c696eb8a
Le Vivilet <levivilet@pm.me>
2026-09-07 11:32:00 +0000

    fix: use i18n strings for problems view labels (#328)

--- packages/problems-view/src/parts/GetProblemsFilterVirtualDom/GetProblemsFilterVirtualDom.ts
+++ packages/problems-view/src/parts/GetProblemsFilterVirtualDom/GetProblemsFilterVirtualDom.ts
@@ -7,6 +7,7 @@ import { getFilterBadgeDom } from '../GetFilterBadgeDom/GetFilterBadgeDom.ts'
 import { getInputBoxVirtualDom } from '../GetInputBoxVirtualDom/GetInputBoxVirtualDom.ts'
 import * as InputName from '../InputName/InputName.ts'
 import * as MaskIcon from '../MaskIcon/MaskIcon.ts'
+import * as ProblemStrings from '../ProblemStrings/ProblemStrings.ts'
 
 const getChildCount = (badgeText: string | undefined): number => {
   let childCount = 0
@@ -30,7 +31,7 @@ export const getProblemsFilterVirtualDom = (action: ViewletAction): readonly Vir
     ...GetActionButtonVirtualDom.getActionButtonVirtualDom({
       command: 'more filters',
       icon: MaskIcon.Filter,
-      id: 'more filters', // TODO use i18n string
+      id: ProblemStrings.moreFilters(),
     }),
   ]
 }
